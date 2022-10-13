78774e99d0109134b417ead1071dd83411f9cdef
MorePog <95282692+MorePog@users.noreply.github.com>
2022-10-13 20:48:33 +0200

    initial commit

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController
+{
+    public function index()
+    {
+        return new Response("HI!");
+    }
+}
