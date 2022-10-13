2ce8187ab10e3361a34526700e84e40ebd820ce0
MorePog <95282692+MorePog@users.noreply.github.com>
2022-10-13 21:45:44 +0200

    basic templating with twig

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     private array $messages = [
         "Hello", "Hi", "Bye", "Welcome"
@@ -18,7 +19,9 @@ class HelloController
 
     public function index(int $limit): Response
     {
-        return new Response(implode(',', array_slice($this->messages, 0, $limit)));
+        return $this->render("hello/index.html.twig", [
+            'messages' => implode(',', array_slice($this->messages, 0, $limit))
+        ]);
     }
 
     /**
@@ -28,6 +31,11 @@ class HelloController
     #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
     public function showOne($id): Response
     {
-        return new Response($this->messages[$id]);
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+        );
     }
 }
