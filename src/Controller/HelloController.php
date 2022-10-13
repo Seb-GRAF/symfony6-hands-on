6413f47522eb5f7429f4086689e9ca3efa373d93
MorePog <95282692+MorePog@users.noreply.github.com>
2022-10-13 21:15:09 +0200

    routes with regex slugs

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,16 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class HelloController
 {
     private array $messages = [
-        "Hello", "Hi", "Bye",
+        "Hello", "Hi", "Bye", "Welcome"
     ];
 
-    #[Route('/', name: 'app_index')]
-    public function index(): Response
+    /**
+     * Route with regex validation from PHP 8 annotations 
+     */
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+
+    public function index(int $limit): Response
     {
-        return new Response(implode(',', $this->messages));
+        return new Response(implode(',', array_slice($this->messages, 0, $limit)));
     }
 
-    #[Route('/messages/{id}', name: 'app_show_one')]
+    /**
+     * Route with regex validation so the app doesn't crash (shows 404 when using a string instead of an int) 
+     */
+
+    #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
     public function showOne($id): Response
     {
         return new Response($this->messages[$id]);
