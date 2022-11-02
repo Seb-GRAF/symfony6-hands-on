6b4ff3fa2542b33eec90ca4fbe7ece4f13db2926
MorePog <95282692+MorePog@users.noreply.github.com>
2022-11-02 11:01:54 +0100

    Added edit route and added reusable form partial

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
-use DateTime;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
 {
@@ -25,5 +27,67 @@ class MicroPostController extends AbstractController
     return $this->render('micro_post/show.html.twig', [
       'post' => $post,
     ]);
+  }
+
+  #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+  public function add(Request $request, MicroPostRepository $posts): Response
+  {
+    $microPost = new MicroPost();
+    $form = $this->createFormBuilder($microPost)
+      ->add('title')
+      ->add('text')
+      // ->add('submit', SubmitType::class, ['label' => 'Save'])
+      ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $post = $form->getData();
+      $post->setCreated(new DateTime());
+      $posts->save($post, true);
+
+      // Add a flash
+      $this->addFlash('success', 'Your micro post has been added');
+
+      // Redirect
+      return $this->redirectToRoute('app_micro_post');
+    }
+
+    return $this->renderForm(
+      'micro_post/add.html.twig',
+      [
+        'form' => $form
+      ]
+    );
+  }
+
+  #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+  public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
+  {
+    $form = $this->createFormBuilder($post)
+      ->add('title')
+      ->add('text')
+      // ->add('submit', SubmitType::class, ['label' => 'Save'])
+      ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $post = $form->getData();
+      $posts->save($post, true);
+
+      // Add a flash
+      $this->addFlash('success', 'Your micro post has been successfully edited');
+
+      // Redirect
+      return $this->redirectToRoute('app_micro_post');
+    }
+
+    return $this->renderForm(
+      'micro_post/add.html.twig',
+      [
+        'form' => $form
+      ]
+    );
   }
 }
